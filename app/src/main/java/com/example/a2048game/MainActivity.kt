c7f94464e9a7623a288d890e0ad98c0cadf652c8
221RDB093 <125742217+221RDB093@users.noreply.github.com>
2024-04-17 00:33:15 +0300

    first commit for game of 15

--- app/src/main/java/com/example/a2048game/MainActivity.kt
+++ app/src/main/java/com/example/a2048game/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.example.a2048game
 
+import android.graphics.Color
 import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageView
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import kotlin.random.Random
@@ -17,38 +19,36 @@ class MainActivity : AppCompatActivity() {
         R.id.tile41, R.id.tile42, R.id.tile43, R.id.tile44,
     )
 
+    private val tilesMap = mapOf<String, Int>(
+
+        "R.id.tile11" to 1, "R.id.tile12" to 2, "R.id.tile13" to 3, "R.id.tile14" to 4,
+        "R.id.tile21" to 5, "R.id.tile22" to 6, "R.id.tile23" to 7, "R.id.tile24" to 8,
+        "R.id.tile31" to 9, "R.id.tile32" to 10, "R.id.tile33" to 11, "R.id.tile34" to 12,
+        "R.id.tile41" to 13, "R.id.tile42" to 14, "R.id.tile43" to 15, "R.id.tile44" to 0,
+        ) // Maybe should be <String, Int> and later key is parsed to int..?
+
+//    private val tilesMap2 = mutableMapOf<Int, Int>(
+//        R.id.tile11 to 1, R.id.tile12 to 2, R.id.tile13 to 3, R.id.tile14 to 4,
+//        R.id.tile21 to 5, R.id.tile22 to 6, R.id.tile23 to 7, R.id.tile24 to 8,
+//        R.id.tile31 to 9, R.id.tile32 to 10, R.id.tile33 to 11, R.id.tile34 to 12,
+//        R.id.tile41 to 13, R.id.tile42 to 14, R.id.tile43 to 15, R.id.tile44 to 0,
+//    )
+//
+//    private val tilesMap2 = mutableMapOf<Int, String>(
+//        R.id.tile11 to "1", R.id.tile12 to "2", R.id.tile13 to "3", R.id.tile14 to "4",
+//        R.id.tile21 to "5", R.id.tile22 to "6", R.id.tile23 to "7", R.id.tile24 to "8",
+//        R.id.tile31 to "9", R.id.tile32 to "10", R.id.tile33 to "11", R.id.tile34 to "12",
+//        R.id.tile41 to "13", R.id.tile42 to "14", R.id.tile43 to "15", R.id.tile44 to "",
+//    )
+
+
+
+
+
     private fun generateNewTile() {
         var gameover = false
-        // TODO: Implement game over
-        while(true) {
-            val tile1 = tiles[Random.nextInt(tiles.size)]
-            val tileClr = findViewById<ImageView>(tile1).getDrawable()
-            if (tileClr == null) {
-                val chosen = findViewById<ImageView>(tile1)
-                chosen.setImageResource(R.drawable.tile2)
-                break
+        // TODO: Implement game over (in a different function)
 
-            } // else if (tileClr != null) {
-//                var iterator = 0
-//                for (tile in tiles) {
-//                    if (iterator != tiles.size) {
-//                        val nextTile = findViewById<ImageView>(tile).getDrawable()
-//                        if (nextTile != null) {
-//                            iterator++
-//                        }
-//                    } else {
-//                        println("Game Over!!!")
-//                        gameover = true
-//                        break
-//                    }
-//                    break
-//                }
-//
-//            }
-            else {
-                continue
-            }
-        }
 
     }
 
@@ -56,41 +56,111 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // Atsauce: https://www.baeldung.com/kotlin/list-get-random-item
-        val idx1 = Random.nextInt(tiles.size)
-        var idx2 = Random.nextInt(tiles.size)
-        while(idx1 == idx2)  idx2 = Random.nextInt(tiles.size);
-        val randomTile1 = tiles[idx1]
-        val randomTile2 = tiles[idx2]
+        val tilesMap2 = mutableMapOf<Int, String>(
+            R.id.tile11 to "1", R.id.tile12 to "2", R.id.tile13 to "3", R.id.tile14 to "4",
+            R.id.tile21 to "5", R.id.tile22 to "6", R.id.tile23 to "7", R.id.tile24 to "8",
+            R.id.tile31 to "9", R.id.tile32 to "10", R.id.tile33 to "11", R.id.tile34 to "12",
+            R.id.tile41 to "13", R.id.tile42 to "14", R.id.tile43 to "15", R.id.tile44 to "",
+        )
 
-        val t1 = findViewById<ImageView>(randomTile1)
-        val t2 = findViewById<ImageView>(randomTile2)
-        t1.setImageResource(R.drawable.tile2)
-        t2.setImageResource(R.drawable.tile2)
+        val valuesList = tilesMap2.values.toMutableList()
+
+        // Shuffle the list of values
+        valuesList.shuffle()
+
+        // Update the map with shuffled values
+        tilesMap2.replaceAll { _, _ -> valuesList.removeAt(0) }
+
+
+        tilesMap2.forEach { (key, value) ->
+            println("$key -> $value")
+            val textView = findViewById<TextView>(key)
+            findViewById<TextView>(key).setText(value)
+            if (value == "") {
+                textView.text = value
+                textView.setBackgroundColor(Color.parseColor("#95877C"))
+            } else textView.setBackgroundColor(Color.parseColor("#DDC6B4"))
+
+            findViewById<TextView>(key).setText(value)
+
+        }
+
+
 
         // Button logic
-        val down = findViewById<Button>(R.id.buttonDown)
+
         val right = findViewById<Button>(R.id.buttonRight)
         val left = findViewById<Button>(R.id.buttonLeft)
         val up = findViewById<Button>(R.id.buttonUp)
 
 
+        val down = findViewById<Button>(R.id.buttonDown)
         down.setOnClickListener {
-            Toast.makeText(this, "Shift Down", Toast.LENGTH_SHORT).show()
-            // val some = findViewById<ImageView>(R.id.tile)
+            /// Toast.makeText(this, "Shift Down", Toast.LENGTH_SHORT).show()
+
             // TODO: create shift down operation
-            generateNewTile()
+            tilesMap2.forEach { (key, value) ->
+                if (value == "") {
+                    val textView = findViewById<TextView>(key)
+                    textView.text = value
+                    textView.setBackgroundColor(Color.parseColor("#DDC6B4"))
+                    val currentIndex = tilesMap2.entries.indexOfFirst { it.key == key }
+                    println(currentIndex)
+                    val newKey = tilesMap2.keys.elementAt(currentIndex - 4)
+                    // println(tilesMap2[newKey])
+                    val upperTView = findViewById<TextView>(newKey)
+                    upperTView.setBackgroundColor(Color.parseColor("#95877C"))
+                    findViewById<TextView>(key).setText(tilesMap2[newKey])
+                    val upperValue = tilesMap2.values.elementAt(currentIndex - 4)
+                    // println("aaa: $upperValue")
+                    tilesMap2[key] = upperValue
+                    upperTView.text = ""
+                    tilesMap2[newKey] = ""
+                }
+
+            }
+
 
         }
         right.setOnClickListener {
             Toast.makeText(this, "Shift Right", Toast.LENGTH_SHORT).show()
+//            val seventhEntry = tilesMap.entries.elementAt(6)
+//            println(seventhEntry.key)
+//            println(tilesMap.size)
+            println(tiles[0])
+            println(tiles[1])
+            println(tiles[2])
+
+
         }
         left.setOnClickListener {
             Toast.makeText(this, "Shift Left", Toast.LENGTH_SHORT).show()
         }
         up.setOnClickListener {
-            Toast.makeText(this, "Shift Up", Toast.LENGTH_SHORT).show()
+            // Toast.makeText(this, "Shift Up", Toast.LENGTH_SHORT).show()
+
+            tilesMap2.forEach { (key, value) ->
+                if (value == "") {
+                    val textView = findViewById<TextView>(key)
+                    textView.text = value
+                    textView.setBackgroundColor(Color.parseColor("#DDC6B4"))
+                    val currentIndex = tilesMap2.entries.indexOfFirst { it.key == key }
+                    println(currentIndex)
+                    val newKey = tilesMap2.keys.elementAt(currentIndex + 4)
+                    // println(tilesMap2[newKey])
+                    val upperTView = findViewById<TextView>(newKey)
+                    upperTView.setBackgroundColor(Color.parseColor("#95877C"))
+                    findViewById<TextView>(key).setText(tilesMap2[newKey])
+                    val upperValue = tilesMap2.values.elementAt(currentIndex + 4)
+                    // println("aaa: $upperValue")
+                    tilesMap2[key] = upperValue
+                    upperTView.text = ""
+                    tilesMap2[newKey] = ""
+                }
+
+            }
         }
+
 
     }
 
