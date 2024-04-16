bacea17b2e799febf3e8739cee1803400b7f07bb
221RDB093 <125742217+221RDB093@users.noreply.github.com>
2024-04-16 11:25:20 +0300

    modified board & created tile generator

--- app/src/main/java/com/example/a2048game/MainActivity.kt
+++ app/src/main/java/com/example/a2048game/MainActivity.kt
@@ -1,34 +1,74 @@
 package com.example.a2048game
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.MotionEvent
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import kotlin.random.Random
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.gestures.Orientation
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.offset
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
-import androidx.compose.material.FractionalThreshold
-import androidx.compose.material.rememberSwipeableState
-import androidx.compose.material.swipeable
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalDensity
-import androidx.compose.ui.unit.IntOffset
-import androidx.compose.ui.unit.dp
-import java.util.Scanner
-import kotlin.math.roundToInt
 
 class MainActivity : AppCompatActivity() {
+
+    private val tiles = intArrayOf(
+        R.id.tile11, R.id.tile12, R.id.tile13, R.id.tile14,
+        R.id.tile21, R.id.tile22, R.id.tile23, R.id.tile24,
+        R.id.tile31, R.id.tile32, R.id.tile33, R.id.tile34,
+        R.id.tile41, R.id.tile42, R.id.tile43, R.id.tile44,
+    )
+
+    private fun generateNewTile() {
+        var gameover = false
+        // TODO: Implement game over
+        while(true) {
+            val tile1 = tiles[Random.nextInt(tiles.size)]
+            val tileClr = findViewById<ImageView>(tile1).getDrawable()
+            if (tileClr == null) {
+                val chosen = findViewById<ImageView>(tile1)
+                chosen.setImageResource(R.drawable.tile2)
+                break
+
+            } // else if (tileClr != null) {
+//                var iterator = 0
+//                for (tile in tiles) {
+//                    if (iterator != tiles.size) {
+//                        val nextTile = findViewById<ImageView>(tile).getDrawable()
+//                        if (nextTile != null) {
+//                            iterator++
+//                        }
+//                    } else {
+//                        println("Game Over!!!")
+//                        gameover = true
+//                        break
+//                    }
+//                    break
+//                }
+//
+//            }
+            else {
+                continue
+            }
+        }
+
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        // Atsauce: https://www.baeldung.com/kotlin/list-get-random-item
+        val idx1 = Random.nextInt(tiles.size)
+        var idx2 = Random.nextInt(tiles.size)
+        while(idx1 == idx2)  idx2 = Random.nextInt(tiles.size);
+        val randomTile1 = tiles[idx1]
+        val randomTile2 = tiles[idx2]
+
+        val t1 = findViewById<ImageView>(randomTile1)
+        val t2 = findViewById<ImageView>(randomTile2)
+        t1.setImageResource(R.drawable.tile2)
+        t2.setImageResource(R.drawable.tile2)
+
+        // Button logic
         val down = findViewById<Button>(R.id.buttonDown)
         val right = findViewById<Button>(R.id.buttonRight)
         val left = findViewById<Button>(R.id.buttonLeft)
@@ -37,6 +77,10 @@ class MainActivity : AppCompatActivity() {
 
         down.setOnClickListener {
             Toast.makeText(this, "Shift Down", Toast.LENGTH_SHORT).show()
+            // val some = findViewById<ImageView>(R.id.tile)
+            // TODO: create shift down operation
+            generateNewTile()
+
         }
         right.setOnClickListener {
             Toast.makeText(this, "Shift Right", Toast.LENGTH_SHORT).show()
@@ -49,6 +93,8 @@ class MainActivity : AppCompatActivity() {
         }
 
     }
+
+
 
 
 }
